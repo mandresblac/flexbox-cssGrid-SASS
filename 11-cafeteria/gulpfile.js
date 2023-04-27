588810d70db17f55775c27d014670b2ade6be2cb
manuel Blanco <mandresblac@gmail.com>
2023-04-26 23:15:47 -0500

    commit 36

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -1,7 +1,13 @@
 const { src, dest, watch, series, parallel } = require("gulp");
+
+// CSS y SASS
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+
+// Imagenes
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
 
 function css(done) {
     // Compilar sass
@@ -16,13 +22,29 @@ function css(done) {
     done();
 }
 
+function imagenes() {
+    return src("src/img/**/*")
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'))
+        
+}
+
+function versionWebp() {
+    return src("src/img/**/*.{png,jpg}")
+        .pipe(webp())
+        .pipe(dest("build/img"))
+}
+
 function dev() {
-    watch("src/scss/app.scss", css); // watch toma 2 valores, 1- a que le tiene que poner atención, 2- que funcion va a ejecutar.
+    watch("src/scss/**/*.scss", css); // watch toma 2 valores, 1- a que le tiene que poner atención, 2- que funcion va a ejecutar.
+    watch("src/img/**/*", imagenes);
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
 
 // series - Se inicia una tarea y hasta que finaliza inicia la siguiente.
 // parallel - Todas inician al mismo tiempo.
-exports.default = series(css, dev);
+exports.default = series(imagenes, versionWebp, css, dev);
