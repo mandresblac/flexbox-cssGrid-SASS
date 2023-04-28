6e5da3295408a224251827d06abd359ffa0f93ee
manuel Blanco <mandresblac@gmail.com>
2023-04-27 22:20:32 -0500

    commit 37

--- 11-cafeteria/gulpfile.js
+++ 11-cafeteria/gulpfile.js
@@ -8,6 +8,7 @@ const autoprefixer = require("autoprefixer");
 // Imagenes
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
     // Compilar sass
@@ -30,8 +31,20 @@ function imagenes() {
 }
 
 function versionWebp() {
+    const opciones = {
+        quality: 50
+    }
     return src("src/img/**/*.{png,jpg}")
-        .pipe(webp())
+        .pipe(webp(opciones))
+        .pipe(dest("build/img"))
+}
+
+function versionAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src("src/img/**/*.{png,jpg}")
+        .pipe(avif(opciones))
         .pipe(dest("build/img"))
 }
 
@@ -44,7 +57,8 @@ exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
 
 // series - Se inicia una tarea y hasta que finaliza inicia la siguiente.
 // parallel - Todas inician al mismo tiempo.
-exports.default = series(imagenes, versionWebp, css, dev);
+exports.default = series(imagenes, versionWebp, versionAvif, css, dev);
